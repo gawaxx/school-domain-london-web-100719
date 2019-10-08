fe149c59bd00c654e61a265d53fc80bdc820d3f2
jules blanc <jules.blanc@unccub.com>
2019-10-08 15:36:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,12 +11,8 @@ class School
   
   
   def add_student(student_name, grade)
-    if roster.incude?[grade]
-      roster[grade] << student_name
-    else 
-      roster[grade] = []
-      roster[grade] << name
-    end 
+    @roster[grade] ||= []
+    @roster[grade] = student_name
   end 
   
   def grade(grade)
