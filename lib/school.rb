86c37d167404cc73bcef03bf2ce25e68ae6d4c51
jules blanc <jules.blanc@unccub.com>
2019-10-08 14:55:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+class School 
+  
+  def initialize(model)
+    @model = model
+  end 
+  
+  roster = {}
+  
+  def add_student(name, grade)
+    if roster.incude?[grade]
+      roster[grade] << name
+    else 
+      roster[grade] = []
+      roster[grade] << name
+  end 
+  
+  def grade(grade)
+    thestudents = []
+    roster.each do |key, value|
+      if value == 9
+        thestudents.push(key)
+      end
+    end 
+    return thestudents
+  end 
+  
+  def sort 
+    roster.each do |key, value|
+      return roster[:key]
+    end   
+      
+  end 
+  
+end   
